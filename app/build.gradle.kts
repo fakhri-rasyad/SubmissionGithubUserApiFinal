4710d6ad19c577825cd5ca54a84396d9446560bd
fakhri-rasyad <86504619+fakhri-rasyad@users.noreply.github.com>
2024-03-09 11:06:38 +0800

    chore: penambahan depedensi

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,11 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.fragment:fragment-ktx:1.6.2")
+    implementation("androidx.viewpager2:viewpager2:1.0.0")
+    implementation("androidx.activity:activity-ktx:1.8.2")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
