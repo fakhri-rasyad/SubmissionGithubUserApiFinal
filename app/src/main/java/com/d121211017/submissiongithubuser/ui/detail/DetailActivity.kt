70cc3a4a53bb20ce4809f9d70673c5ad890c1b82
fakhri-rasyad <86504619+fakhri-rasyad@users.noreply.github.com>
2024-03-12 10:58:08 +0800

    feat : bagian info halaman detail

---
+++ app/src/main/java/com/d121211017/submissiongithubuser/ui/detail/DetailActivity.kt
@@ -0,0 +1,12 @@
+package com.d121211017.submissiongithubuser.ui.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.d121211017.submissiongithubuser.R
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
